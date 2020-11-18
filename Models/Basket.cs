70d1a3e068d5c316c9ab31f238dcef416dc45699
Denis <me@gg.com>
2020-11-18 11:35:47 +0200

    Basket_model

---
+++ Models/Basket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarShop.Models
+{
+    public class Basket
+    {
+        [Key]
+        public int Id { get; private set; }
+
+        public Announcement Announcement { get; private set; }
+
+        public Account Account { get; private set; }
+
+        public Basket()
+        {
+
+        }
+    }
+}
