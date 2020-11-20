6e27f08e2fc0253ce684c214aea2f2f70af1570a
Denis <me@gg.com>
2020-11-20 18:07:40 +0200

    ModelAndDb_fix

--- Models/CarShopDb.cs
+++ Models/CarShopDb.cs
@@ -10,17 +10,17 @@ namespace CarShop.Models
     {
         public CarShopDb() :base("connstr")
         {
-
+           
         }
 
-        public virtual DbSet<Account> Accounts { get; set; }
-        public virtual DbSet<Announcement> Announcements { get; set; }
-        public virtual DbSet<Basket> Baskets { get; set; }
-        public virtual DbSet<CarBodyStyle> CarBodyStyles { get; set; }
-        public virtual DbSet<CarBrand> CarBrands { get; set; }
-        public virtual DbSet<CarEngineType> CarEngineTypes { get; set; }
-        public virtual DbSet<CarModel> CarModels { get; set; }
-        public virtual DbSet<CarTransmissionType> CarTransmissionTypes { get; set; }
-        public virtual DbSet<Comment> Comments { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<CarBodyStyle> CarBodyStyles { get; set; }
+        public DbSet<CarBrand> CarBrands { get; set; }
+        public DbSet<CarEngineType> CarEngineTypes { get; set; }
+        public DbSet<CarModel> CarModels { get; set; }
+        public DbSet<CarTransmissionType> CarTransmissionTypes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
