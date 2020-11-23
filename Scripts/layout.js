9a40e53533077d4b204bd3aa395105cbe789a3bb
Denis <me@gg.com>
2020-11-23 21:50:41 +0200

    layout_modifyied

---
+++ Scripts/layout.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+    $(function () {
+        UpdateNav();
+    });
+
+    $(window).resize(function () {
+        UpdateNav();
+    });
+
+    $('.btn-cab').hover(function () {
+        $(this).attr("src","Content/Images/btnloginhover.svg");
+    }, function () {
+        $(this).attr("src", "Content/Images/btnlogin.svg");
+    });
+
+});
+
+function UpdateNav() {
+    if ($('.navbar-toggler').is(":visible") && $('nav').hasClass("navbar-padding")) {
+        $('nav').removeClass("navbar-padding");
+    }
+    else if (!$('.navbar-toggler').is(":visible")) {
+        $('nav').addClass("navbar-padding");
+    }
+}
